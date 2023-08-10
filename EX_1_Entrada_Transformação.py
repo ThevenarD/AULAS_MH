977fb80e7512ea7c73373a5582f7b5e6845f9906
ThevenarD <88249578+ThevenarD@users.noreply.github.com>
2023-08-10 16:54:36 -0300

    Add files via upload

---
+++ EX_1_Entrada_Transformação.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Digite um Número: "))
+numero2 = int(input("Digite outro Número: "))
+
+print("\nO Primeiro Número Digitado Foi: ",numero1)
+print("\nO Segundo Número Digitado Foi: ", numero2)
+print("\nA Soma Entre os Dois Números é: ",numero1  - numero2)
